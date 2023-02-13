2fac0010ce28973c1c7f2c07e6832636f39f6647
kostya vinogradov <92314213+Eisim@users.noreply.github.com>
2023-02-13 14:14:53 +0300

    Delete build2 directory

--- build2/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Eisim/laba5/gtest
-# Build directory: C:/Users/Eisim/laba5/build2/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build2/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Eisim/laba5/test
-# Build directory: C:/Users/Eisim/laba5/build2/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_polynoms "test_polynoms")
-set_tests_properties(test_polynoms PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Eisim/laba5/test/CMakeLists.txt;9;add_test;C:/Users/Eisim/laba5/test/CMakeLists.txt;0;")
