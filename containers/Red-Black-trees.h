9443212ee8841d1dfbbe6ff4a0aabb0334cb613f
Eisim <koctik.w@mail.ru>
2023-03-17 20:58:41 +0300

    .

--- containers/Red-Black-trees.h
+++ containers/Red-Black-trees.h
@@ -1,16 +1,17 @@
 #include<iostream>
 #include<string>
-#include <iomanip> 
+
 template<typename T, typename D>
 class Tree {
 	struct Node {
 		Node* left;
 		Node* right;
+		int height;
 		T key;
 		D element;
-		Node() { left = nullptr; right = nullptr; }
-		Node(T key, D elem) :key(key), element(elem) { left = nullptr; right = nullptr; };
-		Node(const Node& n) :key(key), element(n.element), left(n.left), right(n.right) {};
+		Node() { left = nullptr; right = nullptr; height = 0; }
+		Node(T key, D elem) :key(key), element(elem) { left = nullptr; right = nullptr; height = 0; };
+		Node(const Node& n) :key(key), element(n.element), left(n.left), right(n.right), height(n.height) {};
 		Node& operator=(const Node& n) {
 			key = n.key;
 			element = n.element;
@@ -29,45 +30,53 @@ class Tree {
 		}
 	};
 	Node* root;
-	int height;
 
-
-	Node* insert(T key, D elem, Node* t) {
+	Node* prinsert(T key, D elem, Node* t) {
 		if (t == nullptr)
 			return new Node(key, elem);
 		if (t->key == key) return t;
-		if (t->key > key) t->left = insert(key, elem, t->left);
-		if (t->key < key) t->right = insert(key, elem, t->right);
-		return t;
-	}
-	Node* find(T key, Node* t) {
-		if (t == nullptr)return t;
-		if (t->key > key)return find(key, t->left);
-		else if (t->key < key)return find(key, t->right);
-		return t;
-	}
+		if (t->key > key) {
+			t->left = prinsert(key, elem, t->left);
+			balance(t);
+		}
+		if (t->key < key) {
+			t->right = prinsert(key, elem, t->right);
+		}
 
-	Node* eraseMax(Node* t) {
-		if (t->right)t->right = eraseMax(t->right);
+		return t;
+	}
+	Node* prfind(T key, Node* t) {
+		if (t == nullptr)return t;
+		if (t->key > key)return prfind(key, t->left);
+		else if (t->key < key)return prfind(key, t->right);
+		return t;
+	}
+	Node* preraseMax(Node* t) {
+		if (t->right)t->right = preraseMax(t->right);
 		else
 			return t->left;
 		return t;
 	}
-	Node* erase(T key, Node* t) {
+	Node* prerase(T key, Node* t) {
 		if (t == nullptr) return t;
-		if (t->key > key) t->left = erase(key, t->left);
-		else if (t->key < key) t->right = erase(key, t->right);
+		if (t->key > key) {
+			t->left = prerase(key, t->left);
+			t->height--;
+		}
+		else if (t->key < key) {
+			t->right = prerase(key, t->right);
+			t->height--;
+		}
 		else {
 			Node* max = findMax(t->left);
-			Node* left = t->left; *right = t->right;
+			Node* left = t->left, * right = t->right;
 			delete t;
 			max->right = right;
-			max->left = eraseMax(left);
+			max->left = preraseMax(left);
 			return max;
 		}
 		return t;
 	}
-
 	int prtakeSize(int& res, Node* cur_node)const {
 		res++;
 		if (cur_node->right)
@@ -78,18 +87,56 @@ class Tree {
 		return res;
 	};
 	int prtakeHeight(Node* n) const {
-		int h = 0, l = 0, r = 0;
+		int l = 0, r = 0;
+		if (!n)return 0;
 		if (n->left)l = prtakeHeight(n->left);
 		if (n->right)r = prtakeHeight(n->right);
-		return h = ((l > r) ? l : r) + 1;
+		return ((l > r) ? l : r) + 1;
 	}
+	Node* prfindNext(Node* cur_node, bool flag = false) {
+		if (!flag && cur_node->right) cur_node = prfindNext(cur_node->right, true);
+		else if (flag && cur_node->left)cur_node = prfindNext(cur_node->left, true);
+		return cur_node;
+	}
+	Node* prfindPrev(Node* cur_node, bool flag = false) {
+		if (!flag && cur_node->left) cur_node = prfindPrev(cur_node->left, true);
+		else if (flag && cur_node->right)cur_node = prfindPrev(cur_node->right, true);
+		return cur_node;
+	}
+	Node* balance(Node* t) {
+		int balfunc = takeHeight(t->right) - takeHeight(t->left);
+		if (balfunc == 2) {
+			if (takeHeight(t->right->right) - takeHeight(t->right->left) < 0)
+				rightRotate(t->right);
+			leftRotate(t);
+		}
+		else if (balfunc == -2) {
+			if (takeHeight(t->left->left) - takeHeight(t->left->right) < 0)
+				leftRotate(t->left);
+			rightRotate(t);
+		}
+		return t;
+	}
+
+	void rightRotate(Node* t) {
+
+		Node* new_local_root = t->left;
+		t->left = new_local_root->right;
+		new_local_root->right = t;
+	}
+	void leftRotate(Node* t) {
+		Node* new_local_root = t->right;
+		t->right = new_local_root->left;
+		new_local_root->left = t;
+	}
+
 public:
+	Tree() { root = nullptr; }
 
 	Tree(T key, D elem) {
-		height = 0;
 		root = new Node(key, elem);
 	}
-	Tree(const Tree& t) { root = t.root; height = 0; }
+	Tree(const Tree& t) { root = t.root; }
 	int takeSize() const {
 		int result = 0;
 		result = prtakeSize(result, root);
@@ -98,8 +145,8 @@ public:
 
 	Tree& operator= (const Tree& t) { root = t.root; return*this; }
 
-	int takeHeight() const {
-		return prtakeHeight(root);
+	int takeHeight(Node* t) const {
+		return prtakeHeight(t);
 	}
 
 	Node* findMax(Node* t)
@@ -112,13 +159,30 @@ public:
 		if (t == nullptr || t->left == nullptr) return t;
 		else findMax(t->left);
 	}
-	bool find(T key) {
-		return !(!find(key, root));
+	Node* find(T key) {
+		return prfind(key, root);
 	}
 	void insert(T key, D elem) {
-		this->root = insert(key, elem, root);
+		this->root = prinsert(key, elem, root);
+	}
+	void erase(T key) {
+		if (key != root->key)
+			prerase(key, root);
+		Node* right = root->right;
+		Node* newleft = findMin(root->right);
+		newleft->left = root->left;
+		delete root;
+		root = right;
 	}
 
+	Node* findNext(T key) {
+		Node* node = find(key);
+		return prfindNext(node);
+	}
+	Node* findPrev(T key) {
+		Node* node = find(key);
+		return prfindPrev(node);
+	}
 
 	friend std::ostream& recOut(std::ostream& ostream, const Node* n) {
 		ostream << n << "\n";
--- samples/sample_polynoms.cpp
+++ samples/sample_polynoms.cpp
@@ -1,10 +1,9 @@
 ﻿#include <iostream>
+#include"Red-Black-trees.h"
 #include"monom.h"
-
 int main()
 {
-	Monom m1("-6x^3y^4z^5"),
-		m2("2x^3y^4z^5");
-	std::cout << (m1 <= m2);
-	return 0;
+	//-2.6x^2y
+	Monom m("-2.6x^2y");
+	std::cout << m;
 }
