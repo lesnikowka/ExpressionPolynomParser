8350789cd7ba52d9a33ffb1301c65f3f69d2c067
Eisim <koctik.w@mail.ru>
2023-04-01 19:59:51 +0300

    .

--- samples/sample_polynoms.cpp
+++ samples/sample_polynoms.cpp
@@ -3,27 +3,14 @@
 #include"monom.h"
 #include"hash-table(chaining).h"
 #include"expression.h"
+#include"unordered-table.h"
+#include"hash-table(open-adressing).h"
 int main()
 {
-	RBTree<int,int> t;
-	t.emplace(2,2);
-	t.emplace(5,5);
-	t.emplace(1, 1);
-	t.emplace(7, 7);
-
-
-	for (auto it : t) {
-		std::cout << &it<< '\n';
-	}
-	
-	//Expression<RBTree<std::string,Polynom>> e;
-	//e.addExp("a=xyz");
-	//e.addExp("a");
-	//e.addExp("b=xyz");
-	//e.addExp("b");
-	//e.addExp("b=2xyz");
-	//e.addExp("b+a");
-	//std::cout << e;
-
+	HashTableC<int, int> ht;
+	for(int i=0;i<9;i++)
+	ht.emplace(i, i);
+	ht.erase(1000);
+	std::cout << ht;
 	return 0;
 }
