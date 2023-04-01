205324a323c339dac73cc1c4dc5ec1588e1522b0
Eisim <koctik.w@mail.ru>
2023-04-02 02:48:03 +0300

    .

--- samples/sample_polynoms.cpp
+++ samples/sample_polynoms.cpp
@@ -4,15 +4,20 @@
 #include"hash-table(chaining).h"
 #include"expression.h"
 #include"unordered-table.h"
+#include"ordered-table.h"
 #include"hash-table(open-adressing).h"
+#include"AVL-tree.h"
 int main()
 {
-	//RBTree<std::string, Polynom> t;
-	//t.emplace("tmp1",Polynom("xyz"));
-	//t.emplace("tmp2",Polynom("xyz"));
-	//std::cout<<t;
-	Expression<RBTree<std::string, Polynom>> a;
-	a.addExp("a===2");
-	a.addExp("a");
+	RBTree<std::string, Polynom> t;
+	t.emplace("a",Polynom("x"));
+	//t.emplace("b",Polynom("y"));
+	t.erase("a");
+	for (auto it : t) {
+		std::cout << &it;
+	}
+	//Expression<RBTree<std::string, Polynom>> a;
+	//a.addExp("a===2");
+	//a.addExp("a");
 	return 0;
 }
