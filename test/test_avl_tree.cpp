fdf26122ecdf4e6d04ea46371de7c980148cd33a
lesnikowka <92714965+lesnikowka@users.noreply.github.com>
2023-04-03 12:40:19 +0300

    Update test_avl_tree.cpp

--- test/test_avl_tree.cpp
+++ test/test_avl_tree.cpp
@@ -9,18 +9,19 @@ bool is_equal(A v, B t) {
 		if (t.find(i.first) == t.end()) return false;
 	}
 	for (auto i : t) {
-		if(std::find(v.begin(), v.end(), i) == v.end()) return false;
+		if (std::find(v.begin(), v.end(), i) == v.end()) return false;
 	}
 	return true;
 }
 
-class AVL_fixture : public testing::Test{
+class AVL_fixture : public testing::Test {
 protected:
 	AVLTree<int, int> t;
 	std::vector<std::pair<int, int>> v = { {4, 5}, {3, 7}, {10, 17}, {8, 13}, {12, 7}, {13, 14} , {1, 10} };
 	std::vector<std::pair<int, int>> v2 = { {4, 0}, {3, 0}, {10, 0}, {8, 0}, {12, 0}, {13, 0} };
 	std::vector<std::pair<int, int>> v3 = { {4,0},{3,0},{10,0},{1,0},{2,0}, {-1,0} };
 	std::vector<std::pair<int, int>> v4 = { {4, 0}, {3, 0}, {10, 0}, {7, 0}, {12, 0}, {6, 0}, {8,0}, {9,0} };
+	std::vector<std::pair<int, int>> v5 = { {-4, 0}, {-3, 0}, {-10, 0}, {-7, 0}, {-12, 0}, {-6, 0}, {-8,0}, {-9,0} };
 };
 
 TEST_F(AVL_fixture, size_is_correct_on_empty_tree) {
@@ -35,9 +36,9 @@ TEST_F(AVL_fixture, size_is_correct_in_common_case) {
 	EXPECT_EQ(t.size(), v.size());
 }
 TEST_F(AVL_fixture, size_is_correct_after_erasing) {
-	t.emplace(1,1);
-	t.emplace(2,2);
-	t.erase(1); 
+	t.emplace(1, 1);
+	t.emplace(2, 2);
+	t.erase(1);
 	t.erase(2);
 
 	EXPECT_EQ(t.size(), 0);
@@ -47,7 +48,7 @@ TEST_F(AVL_fixture, can_insert_elements) {
 	for (auto i : v) {
 		t.insert(i);
 	}
-	
+
 	bool equal = is_equal<std::vector<std::pair<int, int>>, AVLTree<int, int>>(v, t);
 	EXPECT_TRUE(equal);
 }
@@ -112,19 +113,19 @@ TEST_F(AVL_fixture, throw_if_erase_element_that_was_not_added) {
 	ASSERT_ANY_THROW(t.erase(9));
 }
 
-TEST_F(AVL_fixture, can_do_simple_left_rotate) {
+TEST_F(AVL_fixture, can_do_simple_left_rotate_during_the_insert) {
 	for (int i = 0; i < v2.size() - 1; i++) {
 		t.insert(v2[i]);
 	}
 
 	EXPECT_EQ(t.height(), 3);
-	
+
 	t.insert(v2[v2.size() - 1]);
 
 	EXPECT_EQ(t.height(), 3);
 }
 
-TEST_F(AVL_fixture, can_do_simple_right_rotate) {
+TEST_F(AVL_fixture, can_do_simple_right_rotate_during_the_insert) {
 	for (int i = 0; i < v3.size() - 1; i++) {
 		t.insert(v3[i]);
 	}
@@ -136,7 +137,7 @@ TEST_F(AVL_fixture, can_do_simple_right_rotate) {
 	EXPECT_EQ(t.height(), 3);
 }
 
-TEST_F(AVL_fixture, can_do_big_left_rotate) {
+TEST_F(AVL_fixture, can_do_big_left_rotate_during_the_insert) {
 	for (int i = 0; i < v4.size() - 1; i++) {
 		t.insert(v4[i]);
 	}
@@ -149,6 +150,15 @@ TEST_F(AVL_fixture, can_do_big_left_rotate) {
 	EXPECT_EQ(t.height(), 4);
 }
 
-TEST_F(AVL_fixture, can_do_big_right_rotate) {
-	ADD_FAILURE();
+TEST_F(AVL_fixture, can_do_big_right_rotate_during_the_insert) {
+	for (int i = 0; i < v5.size() - 1; i++) {
+		t.insert(v5[i]);
+	}
+
+	EXPECT_EQ(t.height(), 3);
+
+	t.insert(v5[v5.size() - 1]);
+
+
+	EXPECT_EQ(t.height(), 4);
 }
